bb1d0a915836f4234a3c443f459833c65bf2a41f
Theempire2025 <erwincespedesv05@gmail.com>
2025-11-11 22:01:12 -0400

    1er

--- app/src/main/java/com/example/ap_basic_map/MainActivity.java
+++ app/src/main/java/com/example/ap_basic_map/MainActivity.java
@@ -3,17 +3,45 @@ package com.example.ap_basic_map;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMAp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        myMAp =googleMap;
+        LatLng santaCruz=new LatLng(-17.78629, -63.18117);
+        myMAp.moveCamera(CameraUpdateFactory.newLatLng(santaCruz));
+        MarkerOptions options = new MarkerOptions().position(santaCruz).title("Santa Cruz Puej");
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        myMAp.addMarker(options);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
